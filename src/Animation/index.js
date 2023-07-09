87bda3c9b05763f6077d7dad532401a666fa5c94
IARA REGINA MARQUES DOS SANTOS <iararmsantos@gmail.com>
2023-07-09 10:00:15 -0400

    Added Responsive Content

--- src/Animation/index.js
+++ src/Animation/index.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import { Box, Button, Typography} from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
+import GoogleIcon from '@mui/icons-material/Google';
+import EmailIcon from '@mui/icons-material/Email';
 
 const Animation = () => {
   return (
     <Box width='100%' height='100vh'>
-        <video 
-          width='100%' 
-          height='70%' 
-          src='/video.mp4' 
-          autoPlay 
-          loop
-          muted
-          playsInline
+      <video
+        width='100%'
+        height='70%'
+        src='/video.mp4'
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+      </video>
+      <Box display='flex' width='100%'>
+        <Typography
+          fontSize={{lg:30, md:24, sm:18, xs:14}}
+          margin='auto'
+          variant='h4'
+          color='black'
+          textAlign='center'
         >
-        </video> 
-        <Box display='flex' width='100%'>
-            <Typography 
-                margin='auto' 
-                variant='h4' 
-                color='black' 
-                textAlign='center'
-            > 
-            Build Your Softwares Free And With Top Notch Quality                  
-            </Typography>
-        </Box>     
-        <Box 
-          width='100%' 
-          display='flex' 
-          justifyContent='center' 
-          margin='auto' 
-          marginTop={5}
-        >
-          <Button sx={{mr: 3}} variant='outlined'>Sign Up with Email</Button>
-          <Button sx={{ml: 3}} variant='contained'>Sign Up with Google</Button>
-        </Box>          
+          Build Your Softwares Free And With Top Notch Quality
+        </Typography>
+      </Box>
+      <Box
+        width='100%'
+        display='flex'
+        justifyContent='center'
+        margin='auto'
+        marginTop={5}
+      >
+        <Button 
+          endIcon={<EmailIcon/>} sx={{ mr: 3 }} variant='outlined'>Sign Up with</Button>
+        <Button color='error' endIcon={<GoogleIcon/>} sx={{ ml: 3 }} variant='contained'>Sign Up with</Button>
+      </Box>
     </Box>
   )
 }
--- src/Content/ContentItem.js
+++ src/Content/ContentItem.js
@@ -1,17 +1,21 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, useMediaQuery, useTheme } from '@mui/material'
 import React from 'react'
 
 const ContentItem = ({ title, description, img, swap }) => {
+  const isMatch = useMediaQuery(useTheme().breakpoints.down('md'));
+
   return (
     <Box
-    bgcolor={!swap && "#fff"}
+      bgcolor={!swap && "#fff"}
       padding={10}
       justifyContent="space-between"
       alignItems="center"
       display="flex"
+      flexDirection={isMatch ? "column" : "row"}
     >
       {swap ? <>  <Box>
         <Typography
+          fontSize={{lg: 32, md: 28, sm: 24, xs: 20}}
           color="#734950"
           padding={3}
           variant='h3'
@@ -19,6 +23,7 @@ const ContentItem = ({ title, description, img, swap }) => {
           {title}
         </Typography>
         <Typography
+          fontSize={{lg: 24, md: 20, sm: 16, xs: 14}}
           padding={3}                    
         >
           {description}
@@ -27,7 +32,8 @@ const ContentItem = ({ title, description, img, swap }) => {
         <img
           src={img}
           alt={title}
-          loading='lazy' width="50%"
+          loading='lazy'
+          width={isMatch ? "100%" : "50%"}
           style={{
             boxShadow: "10px 10px 20px #ccc",
             marginLeft: '10%',
@@ -38,7 +44,8 @@ const ContentItem = ({ title, description, img, swap }) => {
         <img
           src={img}
           alt={title}
-          loading='lazy' width="50%"
+          loading='lazy' 
+          width={isMatch ? "100%" : "50%"}
           style={{
             boxShadow: "10px 10px 20px #ccc",
             marginRight: '10%',
@@ -48,13 +55,15 @@ const ContentItem = ({ title, description, img, swap }) => {
         />
         <Box>
           <Typography
+            fontSize={{lg: 32, md: 28, sm: 24, xs: 20}}
             color="#734950"
             padding={3}
             variant='h3'
           >
             {title}
           </Typography>
-          <Typography                        
+          <Typography  
+            fontSize={{lg: 24, md: 20, sm: 16, xs: 14}}                      
             padding={3}
           >
             {description}
